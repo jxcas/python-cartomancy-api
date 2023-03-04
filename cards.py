893b57bad4d4bb1add1d6df647f436aa92f7fba2
JXC <123920190+jxcas@users.noreply.github.com>
2023-03-04 14:19:54 -0800

    :sparkles: successful GET from Deck of Cards API
    :poop: will need to think about file structure for external API calls vs custom-built

--- cards.py
+++ cards.py
@@ -1,10 +1,16 @@
 from flask import Flask
+import requests, json
 
 app = Flask(__name__)
 
 @app.route("/suits")
 def suits():
     return {"Diamonds":["Jack","Queen","King"]}
+
+@app.route("/deck")
+def deck():
+    get_deck = requests.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+    return get_deck.json()
 
 # tells server to launch along with port
 if __name__ == '__main__':
